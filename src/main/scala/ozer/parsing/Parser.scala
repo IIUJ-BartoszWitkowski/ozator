ad589d29b8e8dc71286aabde0cdf8b2ffd82b1b1
IIUJ-MarcinZolna <zolna.marcin@gmail.com>
2012-11-27 22:04:12 +0100

    i001_z001 Parser and tests

--- src/main/scala/ozer/parsing/Parser.scala
+++ src/main/scala/ozer/parsing/Parser.scala
@@ -4,7 +4,7 @@ import ozer.parsing.{Error => OzerError}
 import scala.util.parsing.combinator.RegexParsers
 
 class Parser extends RegexParsers {
-  val separator = '|'
+  val separator = '\0'
 
   trait Pretty {
     def list(s: String*) = s mkString "\n"
@@ -36,11 +36,100 @@ class Parser extends RegexParsers {
 
     def apply() = list(createHelp, updateHelp , statusHelp)
   }
+  
+  object TagHelp extends Help {
+    override val name = "tag"
+    def addHelp = list(
+      syntax("add TAG NAME TITLE"),
+      syntax("add TAG NAME -- FILE"))
+
+    def rmHelp = list(
+      syntax("rm TAG TITLE"), 
+      syntax("rm TAG -- FILE"),
+      syntax("rm TAG NAME TITLE"),
+      syntax("rm TAG NAME -- FILE"))
+    def repairHelp = list(syntax("repair TITLE"), syntax("repair TAG TITLE"))
+    def autoHelp = syntax("auto")
+      
+    def apply() = list(
+      addHelp,
+      rmHelp,
+      repairHelp,
+      autoHelp)
+  }
+
+  object RmMovieHelp extends Help {
+    override val name = "rm-movie" 
+    def rmMovieHelp = syntax("TITLE...")
+    def tagHelp = syntax("-tag KEY VALUE")
+
+    def apply() = list(rmMovieHelp, tagHelp)
+  } 
+
+  object RmFileHelp extends Help {
+    override val name = "rm-movie" 
+
+    def apply() = syntax("FILE...")
+  } 
+
+  object MoveFileHelp extends Help {
+    override val name = "mv-file" 
+
+    def apply() = syntax("SOURCE DESTINATION")
+  } 
+
+  object MoveMovieHelp extends Help {
+    override val name = "mv-movie" 
+
+    def apply() = syntax("TITLE DIRECTORY")
+  } 
+
+  object MoveAllHelp extends Help {
+    override val name = "mv-all" 
+
+    def apply() = syntax("DIRECTORY")
+  } 
+
+  object LsHelp extends Help {
+    override val name = "ls" 
+    def untaggedHelp = syntax("untagged")
+    def tagHelp = list(
+      syntax("tag TITLE"),
+      syntax("tag -- FILE"))
+    def fileNameHelp = syntax("-L FILE")
+  
+
+    def apply() = list(
+      syntax(""),
+      untaggedHelp,
+      tagHelp)
+  }
+
+  object GrepHelp extends Help {
+    override val name = "grep"
+
+    def apply() = syntax("TAG PATTERN")
+  }
+
+  object FindDuplicatesHelp extends Help {
+    override val name = "find-duplicates"
+
+    def apply() = syntax("")
+  }
 
   object CommandHelp extends Pretty {
     def apply() = list(
       SourceHelp(),
-      DbHelp())
+      DbHelp(),
+      TagHelp(),
+      RmMovieHelp(),
+      RmFileHelp(),
+      MoveMovieHelp(),
+      MoveFileHelp(),
+      MoveAllHelp(),
+      LsHelp(),
+      GrepHelp(),
+      FindDuplicatesHelp())
   }
     
   /*
@@ -51,12 +140,12 @@ class Parser extends RegexParsers {
   def command: Parser[Command] = (
     source 
   | db
-  /*| tag
+  | tag
   | rm
-  | mv
-  | ls
+  | mv 
+  | ls 
   | grep
-  | find_duplicates */
+  | find_duplicates 
   | anything ^^ { case _ => OzerError(CommandHelp()) }
   )
 
@@ -82,28 +171,44 @@ class Parser extends RegexParsers {
   | arg("db")                                             ^^ { case _ => OzerError(DbHelp())          }
   )
 
-  /*
   def tag: Parser[Command] = (
-    arg("tag") ~> arg("auto")
+    tag_auto
   | tag_add
   | tag_rm
+  | tag_repair
+  | arg("tag") ~ anything ^^ { case _ => OzerError(TagHelp()) }
+  )
+
+  def tag_auto: Parser[Command] = (
+    arg("tag") ~> arg("auto") ~> anything ^^ { case _ => OzerError(TagHelp.autoHelp) }
+  | arg("tag") ~> arg("auto")             ^^ { case _ => Tag.Auto                    }
   )
 
   def tag_add: Parser[Command] = (
-    arg("tag") ~> arg("add") ~> (arg(string) ~ arg(string) ~ (arg("--") ~> arg(string)))
-  | arg("tag") ~> arg("add") ~> (arg(string) ~ arg(string) ~  arg(string))
+    arg("tag") ~> arg("add") ~> arg(string) ~> arg(string) ~> arg("--") ~> arg(string) ~> anything ^^ { case _ => OzerError(TagHelp.addHelp)                                  }
+  | arg("tag") ~> arg("add") ~> (arg(string) ~ arg(string) ~ (arg("--") ~> arg(string)))           ^^ { case key ~ value ~ fileName => Tag.AddTagToFile(key, value, fileName) }
+  | arg("tag") ~> arg("add") ~> arg(string) ~> arg(string) ~> arg(string) ~> anything              ^^ { case _ => OzerError(TagHelp.addHelp)                                  }
+  | arg("tag") ~> arg("add") ~> (arg(string) ~ arg(string) ~  arg(string))                         ^^ { case key ~ value ~ title => Tag.AddTagToMovie(key, value, title)      }
+  | arg("tag") ~> arg("add") ~> anything                                                           ^^ { case _ => OzerError(TagHelp.addHelp)                                  }
+  | arg("tag") ~> arg("add")                                                                       ^^ { case _ => OzerError(TagHelp.addHelp)                                  }
   )
 
   def tag_rm: Parser[Command] = (
-    arg("tag") ~> arg("rm") ~> arg(string) 
-    arg("tag") ~> arg("rm") ~> (arg(string) ~ arg(string)))
-    arg("tag") ~> arg("rm") ~> (arg("--") ~> arg(string)))
-    arg("tag") ~> arg("rm") ~> (arg(string) ~ (arg("--") ~> arg(string)))
+    arg("tag") ~> arg("rm") ~> arg(string) ~> arg(string) ~> arg("--") ~> arg(string) ~> anything ^^ { case _ => OzerError(TagHelp.rmHelp)                                    }
+  | arg("tag") ~> arg("rm") ~> (arg(string) ~ arg(string) ~ (arg("--") ~> arg(string)))           ^^ { case key ~ value ~ fileName => Tag.RmTagFromFile(key, value, fileName) }
+  | arg("tag") ~> arg("rm") ~> (arg(string) ~ (arg("--") ~> arg(string)))                         ^^ { case key ~ fileName => Tag.RmAllTagsFromFile(key, fileName)            }
+  | arg("tag") ~> arg("rm") ~> arg(string) ~> arg(string) ~> arg(string) ~> anything              ^^ { case _ => OzerError(TagHelp.rmHelp)                                    }
+  | arg("tag") ~> arg("rm") ~> (arg(string) ~ arg(string) ~ arg(string))                          ^^ { case key ~ value ~ title => Tag.RmTagFromMovie(key, value, title)      }
+  | arg("tag") ~> arg("rm") ~> arg(string) ~ arg(string)                                          ^^ { case key ~ title => Tag.RmAllTagsFromMovie(key, title)                 }
+  | arg("tag") ~> arg("rm") ~ anything                                                            ^^ { case _ => OzerError(TagHelp.rmHelp)                                    }
+  | arg("tag") ~> arg("rm")                                                                       ^^ { case _ => OzerError(TagHelp.rmHelp)                                    }
   )
 
   def tag_repair: Parser[Command] = (
-    arg("tag") ~> arg("repair") ~> arg(string)
-  | arg("tag") ~> arg("repair") ~> arg(string) ~ arg(string) 
+    arg("tag") ~> arg("repair") ~> arg(string) ~> arg(string) ~> anything ^^ { case _ => OzerError(TagHelp.repairHelp)              }
+  | arg("tag") ~> arg("repair") ~> arg(string) ~  arg(string)             ^^ { case key ~ title => Tag.RepairTagInMovie(key, title) }
+  | arg("tag") ~> arg("repair") ~> arg(string)                            ^^ { case title       => Tag.RepairAllTagsInMovie(title)  }
+  | arg("tag") ~> arg("repair")                                           ^^ { case _ => OzerError(TagHelp.repairHelp)              }
   )
 
   def rm: Parser[Command] = (
@@ -111,13 +216,16 @@ class Parser extends RegexParsers {
   | rm_movie
   )
 
-  def rm_file = (
-    arg("rm-file") ~> rep1(arg(string)) 
+  def rm_file: Parser[Command] = (
+    arg("rm-file") ~> rep1(arg(string)) ^^ { case files => Rm.Files(files)     }
+  | arg("rm-file")                      ^^ { case _ => OzerError(RmFileHelp()) }
   )
 
-  def rm_movie = (
-  | arg("rm-movie") ~> title 
-  | arg("rm-movie") ~> -("tag") ~> string ~ string 
+  def rm_movie: Parser[Command] = (
+    arg("rm-movie") ~> arg("-tag") ~> arg(string) ~> arg(string) ~> anything ^^ { case _ => OzerError(RmMovieHelp())                 }
+  | arg("rm-movie") ~> arg("-tag") ~> (arg(string) ~ arg(string))            ^^ { case key ~ value => Rm.MovieByKeyValue(key, value) }
+  | arg("rm-movie") ~> rep1(arg(string))                                     ^^ { case titles => Rm.Movies(titles)                   }
+  | arg("rm-movie")                                                          ^^ { case _ => OzerError(RmMovieHelp())                 }
   )
 
   def mv: Parser[Command] = (
@@ -127,38 +235,54 @@ class Parser extends RegexParsers {
   )
 
   def mv_file: Parser[Command] = (
-    arg("mv-file") ~> arg(string) ~ arg(string)  
+    arg("mv-file") ~> arg(string) ~> arg(string) ~> anything ^^ { case _ => OzerError(MoveFileHelp())                          }
+  | arg("mv-file") ~> arg(string) ~ arg(string)              ^^ { case source ~ destination => Move.File(source, destination) }
+  | arg("mv-file") ~> anything                               ^^ { case _ => OzerError(MoveFileHelp())                          }
+  | arg("mv-file")                                           ^^ { case _ => OzerError(MoveFileHelp())                          }
   )
 
   def mv_movie: Parser[Command] = (
-    arg("mv-movie") ~> title ~ directory ^^
+    arg("mv-movie") ~> arg(string) ~> arg(string) ~> anything ^^ { case _ => OzerError(MoveMovieHelp())                     }
+  | arg("mv-movie") ~> arg(string) ~ arg(string)              ^^ { case title ~ directory => Move.Movie(title, directory) }
+  | arg("mv-movie") ~> anything                               ^^ { case _ => OzerError(MoveMovieHelp())                     }
+  | arg("mv-movie")                                           ^^ { case _ => OzerError(MoveMovieHelp())                     }
   )
 
   def mv_all: Parser[Command] = (
-    arg("mv-all") ~> arg(string) 
+    arg("mv-all") ~> arg(string) ~> anything ^^ { case _ => OzerError(MoveAllHelp())    }
+  | arg("mv-all") ~> arg(string)             ^^ { case directory => Move.All(directory) } 
+  | arg("mv-all")                            ^^ { case _ => OzerError(MoveAllHelp())    }
   )
 
   def ls: Parser[Command] = (
-    arg("ls") ~ arg("untagged")
-  | arg("ls") ~> arg("tag") ~> arg(string)
-  | arg("ls") ~> arg("tag") ~> (arg("--") ~> arg(string))
-  | arg("ls") ~> -("L") ~ arg(string)
-  | arg("ls")
+    arg("ls") ~ arg("untagged") ~ anything                          ^^ { case _ => OzerError(LsHelp.untaggedHelp) }
+  | arg("ls") ~ arg("untagged")                                     ^^ { case _ => Ls.Untagged                    }
+  | arg("ls") ~> arg("tag") ~> arg("--") ~> arg(string) ~> anything ^^ { case _ => OzerError(LsHelp.tagHelp)      }
+  | arg("ls") ~> arg("tag") ~> (arg("--") ~> arg(string))           ^^ { case fileName => Ls.TagsOfFile(fileName) }
+  | arg("ls") ~> arg("tag") ~> arg(string) ~> anything              ^^ { case _ => OzerError(LsHelp.tagHelp)      }
+  | arg("ls") ~> arg("tag") ~> arg(string)                          ^^ { case title => Ls.TagsOfMovie(title)      }
+  | arg("ls") ~> arg("-L") ~> arg(string) ~> anything               ^^ { case _ => OzerError(LsHelp.fileNameHelp) }
+  | arg("ls") ~> arg("-L") ~> arg(string)                           ^^ { case title => Ls.FileName(title)         }
+  | arg("ls") ~> anything                                           ^^ { case _ => OzerError(LsHelp())            }
+  | arg("ls")                                                       ^^ { case _ => Ls.Everything                  }
   )
+
 
   def grep: Parser[Command] = (
-    arg("grep") ~> arg(string) ~ arg(string)
+    arg("grep") ~> arg(string) ~ arg(string) ~ anything ^^ { case _ => OzerError(GrepHelp())          }
+  | arg("grep") ~> arg(string) ~ arg(string)            ^^ { case key ~ pattern => Grep(key, pattern) }
+  | arg("grep") ~> anything                             ^^ { case _ => OzerError(GrepHelp())          }
+  | arg("grep")                                         ^^ { case _ => OzerError(GrepHelp())          }
   )
 
+
   def find_duplicates: Parser[Command] = (
-    arg("find-duplicates")
+    arg("find-duplicates") ~> anything ^^ { case _ => OzerError(FindDuplicatesHelp()) }
+  | arg("find-duplicates")             ^^ { case _ => FindDuplicates                  }
   )
-  */
 
   // helpers
 
-  def -[T](parser: Parser[T]): Parser[T] = arg("-" ~> parser)
-  def any_- : Parser[Any] = arg("-" + string) ~ string*
   def arg[T](parser: Parser[T]): Parser[T] = separator ~> parser <~ separator
   def anything = rep1(arg(string))
   
@@ -181,4 +305,10 @@ class Parser extends RegexParsers {
   }
   
   val DEBUG = false
+}
+
+object Parse extends Parser {
+  def apply(arguments: Seq[String]): Command = {
+    parseArguments(arguments)
+  }
 }
--- src/test/scala/ozer/parsing/ParserSuite.scala
+++ src/test/scala/ozer/parsing/ParserSuite.scala
@@ -25,45 +25,318 @@ class ParserSuite extends FunSuite {
   }
   
   test("source add files") {
-     assert(parser.parseArguments(List("source", "add", "file1", "file2")) == Source.Add(List("file1", "file2")))
+    assert(parser.parseArguments(List("source", "add", "file1", "file2")) == Source.Add(List("file1", "file2")))
   }
   
   test("source rm") {
-     assert(parser.parseArguments(List("source", "rm")) == Error(parser.SourceHelp.rmHelp))
+    assert(parser.parseArguments(List("source", "rm")) == Error(parser.SourceHelp.rmHelp))
   }
   
   test("source rm files") {
-     assert(parser.parseArguments(List("source", "rm", "file1", "file2")) == Source.Rm(List("file1", "file2")))
+    assert(parser.parseArguments(List("source", "rm", "file1", "file2")) == Source.Rm(List("file1", "file2")))
   }
 
   test("db create") {
-     assert(parser.parseArguments(List("db", "create")) == Error(parser.DbHelp.createHelp))
+    assert(parser.parseArguments(List("db", "create")) == Error(parser.DbHelp.createHelp))
   }
 
   test("db create dir") {
-     assert(parser.parseArguments(List("db", "create", "dir")) == Db.Create("dir"))
+    assert(parser.parseArguments(List("db", "create", "dir")) == Db.Create("dir"))
   }
 
   test("db create dir ?") {
-     assert(parser.parseArguments(List("db", "create", "dir", "dir2")) == Error(parser.DbHelp.createHelp))
+    assert(parser.parseArguments(List("db", "create", "dir", "dir2")) == Error(parser.DbHelp.createHelp))
   }
 
   test("db update") {
-     assert(parser.parseArguments(List("db", "update")) == Db.Update)
+    assert(parser.parseArguments(List("db", "update")) == Db.Update)
   }
 
-  
   test("db update ?") {
-     assert(parser.parseArguments(List("db", "update", "anything")) == Error(parser.DbHelp.updateHelp))
+    assert(parser.parseArguments(List("db", "update", "anything")) == Error(parser.DbHelp.updateHelp))
   }
 
   test("db status") {
-     assert(parser.parseArguments(List("db", "status")) == Db.Status)
+    assert(parser.parseArguments(List("db", "status")) == Db.Status)
   }
 
   test("db status ?") {
-     assert(parser.parseArguments(List("db", "status", "anything")) == Error(parser.DbHelp.statusHelp))
+    assert(parser.parseArguments(List("db", "status", "anything")) == Error(parser.DbHelp.statusHelp))
+  }
+
+  test("tag auto") {
+    val args = List("tag", "auto")
+    assert(parser.parseArguments(args) == Tag.Auto)
+  }
+
+  test("tag auto ?") {
+    val args = List("tag", "auto", "any")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.autoHelp))
+  }
+
+  test("tag repair") {
+    val args = List("tag", "repair")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.repairHelp))
   }
   
+  test("tag repair title") {
+    val args = List("tag", "repair", "title")
+    assert(parser.parseArguments(args) == Tag.RepairAllTagsInMovie("title"))
+  }
+
+  test("tag repair tag title") {
+    val args = List("tag", "repair", "tag", "title")
+    assert(parser.parseArguments(args) == Tag.RepairTagInMovie("tag", "title"))
+  }
+
+  test("tag repair tag title ?") {
+    val args = List("tag", "repair", "tag", "title", "anything")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.repairHelp))
+  }
+
+  test("tag rm") {
+    val args = List("tag", "rm")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.rmHelp))
+  }
+
+  test("tag rm tag # from what?") {
+    val args = List("tag", "rm", "tag") 
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.rmHelp))
+  }
+
+  test("tag rm tag movie") {
+    val args = List("tag", "rm", "tag", "movie") 
+    assert(parser.parseArguments(args) == Tag.RmAllTagsFromMovie("tag", "movie"))
+  }
+
+  test("tag rm key value movie") {
+    val args = List("tag", "rm", "key", "value", "movie") 
+    assert(parser.parseArguments(args) == Tag.RmTagFromMovie("key", "value", "movie"))
+  }
+
+  test("tag rm key value movie ?") {
+    val args = List("tag", "rm", "key", "value", "movie", "any") 
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.rmHelp))
+  }
+
+  test("tag rm key -- file") {
+    val args = List("tag", "rm", "key", "--", "file") 
+    assert(parser.parseArguments(args) == Tag.RmAllTagsFromFile("key", "file"))
+  }
+
+  test("tag rm key value -- file") {
+    val args = List("tag", "rm", "key", "value", "--", "file") 
+    assert(parser.parseArguments(args) == Tag.RmTagFromFile("key", "value", "file"))
+  }
+
+  test("tag rm key value -- file ?") {
+    val args = List("tag", "rm", "key", "value", "--", "file", "any") 
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.rmHelp))
+  }
+
+  test("tag add") {
+    val args = List("tag", "add")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.addHelp))
+  }
+
+  test("tag add key # value? title?") {
+    val args = List("tag", "add", "key")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.addHelp))
+  }
+
+  test("tag add key value # title?") {
+    val args = List("tag", "add", "key", "value")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.addHelp))
+  }
+
+  test("tag add key value title") {
+    val args = List("tag", "add", "key", "value", "title")
+    assert(parser.parseArguments(args) == Tag.AddTagToMovie("key", "value", "title"))
+  }
+
+  test("tag add key value title ?") {
+    val args = List("tag", "add", "key", "value", "title", "any")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.addHelp))
+  }
+
+  test("tag add key value -- file") {
+    val args = List("tag", "add", "key", "value", "--", "file")
+    assert(parser.parseArguments(args) == Tag.AddTagToFile("key", "value", "file"))
+  }
+
+  test("tag add key value -- file ?") {
+    val args = List("tag", "add", "key", "value", "--", "file", "any")
+    assert(parser.parseArguments(args) == Error(parser.TagHelp.addHelp))
+  } 
+
+  test("find-duplicates") {
+    val args = List("find-duplicates")
+    assert(parser.parseArguments(args) == FindDuplicates)
+  }
+
+  test("find-duplicates ?") {
+    val args = List("find-duplicates", "any")
+    assert(parser.parseArguments(args) == Error(parser.FindDuplicatesHelp()))
+  }
+
+  test("grep") {
+    val args = List("grep")
+    assert(parser.parseArguments(args) == Error(parser.GrepHelp()))
+  }
+
+  test("grep tag # pattern?") {
+    val args = List("grep", "tag")
+    assert(parser.parseArguments(args) == Error(parser.GrepHelp()))
+  }
+
+  test("grep tag pattern") {
+    val args = List("grep", "tag", "pattern")
+    assert(parser.parseArguments(args) == Grep("tag", "pattern"))
+  }
+
+  test("grep tag pattern ?") {
+    val args = List("grep", "tag", "pattern", "anything")
+    assert(parser.parseArguments(args) == Error(parser.GrepHelp()))
+  }
+
+  test("ls") {
+    val args = List("ls")
+    assert(parser.parseArguments(args) == Ls.Everything)
+  }
+
+  test("ls ?") {
+    val args = List("ls", "lala")
+    assert(parser.parseArguments(args) == Error(parser.LsHelp()))
+  }
+
+  test("ls untagged") {
+    val args = List("ls", "untagged")
+    assert(parser.parseArguments(args) == Ls.Untagged)
+  }
+
+  test("ls untagged ?") {
+    val args = List("ls", "untagged", "blah")
+    assert(parser.parseArguments(args) == Error(parser.LsHelp.untaggedHelp))
+  }
+
+  test("ls -L file") {
+    val args = List("ls", "-L", "title")
+    assert(parser.parseArguments(args) == Ls.FileName("title"))
+  }
+
+  test("ls -L file ?") {
+    val args = List("ls", "-L", "title", "any")
+    assert(parser.parseArguments(args) == Error(parser.LsHelp.fileNameHelp))
+  }
+
+  test("ls tag title") {
+    val args = List("ls", "tag", "title")
+    assert(parser.parseArguments(args) == Ls.TagsOfMovie("title"))
+  }
+
+  test("ls tag title ?") {
+    val args = List("ls", "tag", "title", "asdf")
+    assert(parser.parseArguments(args) == Error(parser.LsHelp.tagHelp))
+  }
+
+  test("ls tag -- file") {
+    val args = List("ls", "tag", "--", "file")
+    assert(parser.parseArguments(args) == Ls.TagsOfFile("file"))
+  }
+
+  test("ls tag -- file ??") {
+    val args = List("ls", "tag", "--", "file", "any")
+    assert(parser.parseArguments(args) == Error(parser.LsHelp.tagHelp))
+  }
+
+  test("rm-file") {
+    val args = List("rm-file")
+    assert(parser.parseArguments(args) == Error(parser.RmFileHelp()))
+  }
+
+  test("rm-file file1 file2") {
+    val args = List("rm-file", "file1", "file2")
+    assert(parser.parseArguments(args) == Rm.Files(List("file1", "file2")))
+  }
+
+  test("rm-movie") {
+    val args = List("rm-movie")
+    assert(parser.parseArguments(args) == Error(parser.RmMovieHelp()))
+  }
+
+  test("rm-movie title1 title2") {
+    val args = List("rm-movie", "title1", "title2")
+    assert(parser.parseArguments(args) == Rm.Movies(List("title1", "title2")))
+  }
+
+  test("rm-movie -tag key value") {
+    val args = List("rm-movie", "-tag", "key", "value")
+    assert(parser.parseArguments(args) == Rm.MovieByKeyValue("key", "value"))
+  }
+
+  test("rm-movie -tag key Zvalue ?") {
+    val args = List("rm-movie", "-tag", "key", "value", "any")
+    assert(parser.parseArguments(args) == Error(parser.RmMovieHelp()))
+  }
+
+  test("mv-movie") {
+    val args = List("mv-movie")
+    assert(parser.parseArguments(args) == Error(parser.MoveMovieHelp()))
+  }
+
+  test("mv-movie source") {
+    val args = List("mv-movie", "title")
+    assert(parser.parseArguments(args) == Error(parser.MoveMovieHelp()))
+  }
+
+  test("mv-movie source dest") {
+    val args = List("mv-movie", "title", "dest")
+    assert(parser.parseArguments(args) == Move.Movie("title", "dest"))
+  }
+
+  test("mv-movie source dest ?") {
+    val args = List("mv-movie", "title", "dest", "any")
+    assert(parser.parseArguments(args) == Error(parser.MoveMovieHelp()))
+  }
+
+  test("mv-file") {
+    val args = List("mv-file")
+    assert(parser.parseArguments(args) == Error(parser.MoveFileHelp()))
+  }
+
+  test("mv-file source") {
+    val args = List("mv-file", "source")
+    assert(parser.parseArguments(args) == Error(parser.MoveFileHelp()))
+  }
+
+  test("mv-file source dest") {
+    val args = List("mv-file", "source", "dest")
+    assert(parser.parseArguments(args) == Move.File("source", "dest"))
+  }
+
+  test("mv-file source dest ?") {
+    val args = List("mv-file", "source", "dest", "any")
+    assert(parser.parseArguments(args) == Error(parser.MoveFileHelp()))
+  }
+
+  test("mv-all") {
+    val args = List("mv-all")
+    assert(parser.parseArguments(args) == Error(parser.MoveAllHelp()))
+  }
+
+  test("mv-all dest") {
+    val args = List("mv-all", "dest")
+    assert(parser.parseArguments(args) == Move.All("dest"))
+  }
+
+  test("mv-all dest ?") {
+    val args = List("mv-all", "dest", "asdf")
+    assert(parser.parseArguments(args) == Error(parser.MoveAllHelp()))
+  }
+
+  test("xyzzy") {
+     assert(parser.parseArguments(List("xyzzy")) == Error(parser.CommandHelp()))
+  }
+
   def assertp(s: String) = println(s)
 }
